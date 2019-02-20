2a7f80b10fe1e0db12d2471cb5c134145faa1f5c
All4site <35457395+all4site@users.noreply.github.com>
2019-02-20 19:39:05 +0200

    work

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php get_header();?>
-<nav class="navtop">
-  <div class="logo grid"><img src="https://dummyimage.com/100x50.png" alt=""/></div><?php wp_nav_menu(array(
+<nav class="navwrapper">
+  <div class="wrap">
+    <div class="navtop">
+      <div class="logo grid"><img src="<?php echo get_template_directory_uri()?>/img/logo.png" alt=""/></div><?php wp_nav_menu(array(
 	'menu'						=>	'Главное',
 	'container'				=>	'div',
 	'container_class' =>	'wrapmenu grid',
 	'menu_class'			=>	'menu grid',
 	'menu_id'					=>	'menu'
 ));?>
-  <div class="social grid"><a href="#!" class="icon">
-      <div class="fas fa-headphones"></div></a><a href="#!" class="icon">
-      <div class="fab fa-youtube"></div></a><a href="#!" class="icon">
-      <div class="fab fa-twitter"></div></a><a href="#!" class="icon">
-      <div class="fab fa-facebook-f"></div></a><a href="#!" class="icon">
-      <div class="donate grid"></div></a></div>
-  <div class="btn grid"><a href="#!">поддержать</a></div>
+      <div class="socialwrap grid">
+        <div class="social grid"><a href="#!" class="icon">
+            <div class="fas fa-headphones"></div></a><a href="#!" class="icon">
+            <div class="fab fa-youtube"></div></a><a href="#!" class="icon">
+            <div class="fab fa-twitter"></div></a><a href="#!" class="icon">
+            <div class="fab fa-facebook-f"></div></a><a href="#!" class="icon"></a></div>
+      </div>
+      <div class="donate grid">
+        <div class="btn grid"><a href="#!">поддержать</a></div>
+      </div>
+    </div>
+  </div>
 </nav>
 <section class="contnent">
   <div class="wrap">
